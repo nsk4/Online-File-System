0daa318059219262df44684c8a4ae3cc17955208
Nejc Smrkolj Kozelj <nejcsk@hotmail.com>
2016-12-18 01:50:29 +0100

    folder restructuring and initial commit

---
+++ OnlineFileSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineFileSystem.Startup))]
+namespace OnlineFileSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
